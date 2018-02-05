2cd9f3b00ddaa8fdef64cf65f98ee3f83f52df0d
Alessandro Rosso <rosso.alessandro.9@gmail.com>
2018-02-05 11:36:25 +0100

    Mancano solo più li strumenti utilizzati

--- Scripts/script_calcolo_non_invertente_elementi_reattivi.py
+++ Scripts/script_calcolo_non_invertente_elementi_reattivi.py
@@ -1,4 +1,5 @@
 import math
+import matplotlib.pyplot as plt
 
 debug = False
 
@@ -45,8 +46,22 @@ davdb = k*((da/a)+(drin/rin)+(dcin/cin))
 if debug: print ("av dB %f") % avdb
 if debug: print ("dav dB %f") % davdb
 
+print fp
+print fp2
+
 #define the frequency where we'll calc the gain and the error
 f = [300,1000,3000,10000,30000,100000,300000,1000000]
+
+x=[]
+dx=[]
+ph=[]
+dph=[]
+
+#Valori ottenuti dalle misure (necessarie per la generazione del diagramma di bode)
+mis=[7.00,15.30,18.02,17.20,12.21,2.92,-8.89,-26.9]
+dmis=[0.7,0.67,0.62,0.63,0.74,0.69,0.79,1.0]
+misp=[0.00121,0.000754,0.000170,0.503,-1.13,-1.89,-3.77,-3.14]
+dmisp=[0.00006,0.000046,0.000022,0.22,0.06,0.10,0.17,0.19]
 
 #for each frequency defined above
 for i in f:
@@ -64,16 +79,32 @@ for i in f:
 	#calc a usefull costant for the second pole
 	kp2 = (k*i)/(math.pow(fp2,2)*mfp2)
 	if debug: print ("kp2 %f") % kp2
-	#calc the absolute error of the gain given by the frequence uncertainty
+	#calc the absolute error of the gain given by the frequency error
 	ddf = (k/i) * df
 	if debug: print ("ddf %f") % ddf
-	#calc the absolute error of the gain given by the first pole uncertainty
+	#calc the absolute error of the gain given by the first pole error
 	ddfp = (kp/mfp)*(df + ((i/fp)*dfp))
 	if debug: print ("ddfp %f") % ddfp
-	#calc the absolute error of the gain given by the second pole uncertainty
+	#calc the absolute error of the gain given by the second pole error
 	ddfp2 = (kp2/mfp2)*(df + ((i/fp2)*dfp2))
 	if debug: print ("ddfp2 %f") % ddfp2
 	#calc the absolute error of the gain (summing the error calculated before)
 	dav = davdb + ddf + ddfp + ddfp2
-	#printn, in a shitting way, the results
-	print("@%dHz\tAv = (%.2f +- %.2f)dB") % (i, round(av,2), round(dav,2))
+	#calc of the phase
+	pi = math.atan( (1 - (math.pow(i,2)/(fp*fp2))) / (i*(fp2 + fp)/(fp2*fp)))
+        x.append(round(av,2))
+        dx.append(round(dav,2))
+        ph.append(round(pi,2))
+        dph.append(round(0.00,2))
+	print("@%dHz\tAv = (%.2f +- %.2f)dB\tPhase = (%.2f +- %.2f)rad") % (i, x[len(x)-1], dx[len(x)-1],ph[len(ph)-1],dph[-1])
+
+#Rappresento graficamente risultati ottenuti con incertezze
+plt.subplot(211)
+plt.errorbar(f,x,yerr=dx,color="b",linestyle="--")
+plt.errorbar(f,mis,yerr=dmis,color="r",linestyle=":")
+plt.xscale('log')
+plt.subplot(212)
+plt.errorbar(f,ph,yerr=dph,color="b",linestyle="--")
+plt.errorbar(f,misp,yerr=dmisp,color="r",linestyle=":")
+plt.xscale('log')
+plt.show()
